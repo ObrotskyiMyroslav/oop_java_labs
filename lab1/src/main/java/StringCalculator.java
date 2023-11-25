1afbc6152e6e4e5f0f4cf4a59592e9e2585f2551
Myroslav <obrmyroslaw12@gmail.com>
2023-11-25 09:49:34 +0200

    step 3

--- lab1/src/main/java/StringCalculator.java
+++ lab1/src/main/java/StringCalculator.java
@@ -4,7 +4,7 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
-        String[] numbers_array = numbers.split(",");
+        String[] numbers_array = numbers.split("[,\n]");
         for(String elements : numbers_array){
             if(elements.trim().isEmpty()){
                 throw new IllegalArgumentException("Multiple delimiters not allowed.");
@@ -13,17 +13,17 @@ public class StringCalculator {
         int summury = 0;
         for (int i = 0; i < numbers_array.length; i++) {
             summury += Integer.parseInt(numbers_array[i]);
+
         }
         return summury;
 
     }
-    public static void main (String[]args) {
-        String numbers = "1,2,3,4,5";
+    public static void main (String[]args){
+        String numbers = "1,2,3,4,\n5";
         try {
             System.out.println("The summury is: " + new StringCalculator().add(numbers));
-        } catch (IllegalArgumentException error) {
+        }catch(IllegalArgumentException error){
             System.err.println("Mistake: " + error.getMessage());
         }
     }
-
 }
--- lab1/src/test/java/StringCalculatorTest.java
+++ lab1/src/test/java/StringCalculatorTest.java
@@ -7,21 +7,25 @@ public class StringCalculatorTest {
     StringCalculator str = new StringCalculator();
 
     @Test
-    public void test_21() {
+    public void test_31() {
         assertEquals(0, str.add(""));
     }
 
     @Test
-    public void test_22() {
+    public void test_32() {
         assertEquals(1, str.add("1"));
     }
     @Test
-    public void test_23() {
+    public void test_33() {
         assertEquals(3, str.add("1,2"));
     }
     @Test
-    public void test_24() {
-        assertEquals(15, str.add("1,2,3,4,5"));
+    public void test_34() {
+        assertEquals(15, str.add("1,2,3,4\n5"));
+    }
+    @Test
+    public void test_35(){
+        assertThrows(IllegalArgumentException.class, () -> str.add("1,2,3,4,\n5"));
     }
 
 }
