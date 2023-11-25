a65bdbaf515e25e0361214e5b3c9d36bcaa46648
Myroslav <obrmyroslaw12@gmail.com>
2023-11-25 09:55:39 +0200

    step 8-9

--- lab1/src/main/java/StringCalculator.java
+++ lab1/src/main/java/StringCalculator.java
@@ -1,59 +1,119 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class StringCalculator {
-    public int add(String numbers) {
-        if (numbers.isEmpty()) {
+
+    public int add ( String numbers ) {
+        if ( ! numbers.isEmpty ( ) && !(numbers == null)) {
+            Matcher matcher;
+            if ( numbers.startsWith ( "//[" ) ) {
+                Pattern delimiter_standart = Pattern.compile ( "^(//(.|(\\[[^\\]]+\\])+)\\n)?(.|\\s)+$" );
+                matcher = delimiter_standart.matcher ( numbers );
+                matcher.matches ();
+                if ( matcher.group ( 1 ) == null ) {
+                    throw new IllegalArgumentException ( "Some unexpected error with delimiters" );
+                }
+                else{
+                    numbers = custom_delim ( numbers );}
+            }
+
+            if ( numbers.startsWith ( "//" ) ) {
+
+                matcher = Pattern.compile ( "//(.+)\\n(.+)" ).matcher ( numbers );
+                if ( matcher.find ( ) ) {
+                    int customDelimiterStart = numbers.lastIndexOf ( '/' ) + 1;
+                    int end = numbers.indexOf ( '\n' );
+                    String customDelimiter = numbers.substring ( customDelimiterStart , end );
+                    numbers = numbers.substring ( end + 1 ).replace ( customDelimiter , "," );
+                } else if ( !matcher.find ( ) ) {
+                    //
+                }
+            }
+
+            numbers = numbers.replace ( '\n' , ',' );
+
+            if ( numbers.startsWith ( "," ) && numbers.endsWith ( "," ) ) {
+                throw new IllegalArgumentException  ( "String starts or ends with delimiter" );
+            }
+            if ( numbers.contains ( ",," ) ) {
+                throw new IllegalArgumentException  ( "Multiple delimiters not allowed." );
+            }
+            if ( numbers.contains ( ",\n" ) ) {
+                throw new IllegalArgumentException  ( "Multiple delimiters not allowed." );
+            }
+            if ( numbers.contains ( "\n\n" ) ) {
+                throw new IllegalArgumentException  ( "Multiple delimiters not allowed." );
+            }
+            if ( numbers.contains ( "\n," ) ) {
+                throw new IllegalArgumentException  ( "Multiple delimiters not allowed." );
+            }
+            String[] nums = numbers.split ( "," );
+
+            if ( ! numbers.matches ( "^(-?\\d+,?)+$" ) ) {
+                throw new IllegalStateException ( "String has incorrect delimiter" );
+            } else {
+                int sum;
+                List < Integer > negativeNumbers = new ArrayList <> ( );
+                sum = 0;
+                for ( String number : nums ) {
+                    try {
+                        int num = Integer.parseInt ( number );
+                        if ( num < 0 ) {
+                            negativeNumbers.add ( num );
+                        }
+
+                        if ( num <= 1000 ) {
+                            sum += num;
+                        }
+                    } catch ( NumberFormatException e ) {
+                        //Handle non-integer tokens
+                    }
+                }
+
+                if ( ! negativeNumbers.isEmpty ( ) ) {
+                    throw new IllegalArgumentException ( "Negatives not allowed: " + negativeNumbers );
+                } else {
+                    return sum;
+                }
+
+            }
+        } else {
             return 0;
         }
 
-        String delimiter = ",";
-        String numbersWithoutDelimiter = numbers;
+    }
+    public String custom_delim(String numbers) {
 
-        if (numbers.startsWith("//")) {
-            int delimiterStart = numbers.indexOf("[");
-            int delimiterEnd = numbers.indexOf("]");
-            if (delimiterStart != -1 && delimiterEnd != -1) {
-                delimiter = numbers.substring(delimiterStart + 1, delimiterEnd);
-                numbersWithoutDelimiter = numbers.substring(numbers.indexOf("\n") + 1);
+        Set<String> custom = new TreeSet<>(Comparator.reverseOrder());
+        int delimiterEndIndex = numbers.indexOf("]\n");
+        String[] delimiters = numbers.substring(3, delimiterEndIndex).split("]\\[");
+
+        for (String delimiter : delimiters) {
+            if (!delimiter.isEmpty()) {
+                custom.add(delimiter);
             }
         }
-
-        String[] numbersArray = numbersWithoutDelimiter.split("\\Q" + delimiter + "\\E|,|\n");
-
-        int sum = 0;
-        List<Integer> negativeNumbers = new ArrayList<>();
-
-        for (String element : numbersArray) {
-            if(element.trim().isEmpty()) {
-                throw new IllegalArgumentException("Multiple delimiters not allowed.");
-            }
-            if (!element.trim().isEmpty()) {
-                int num = Integer.parseInt(element);
-
-                if (num < 0) {
-                    negativeNumbers.add(num);
-                }
-                if(num > 1000){
-                    continue;
-                }
-                sum += num;
-            }
+        numbers = numbers.substring(delimiterEndIndex+ 2);
+        custom.add("\n");
+        for (String delimiter : custom) {
+            numbers = numbers.replace(delimiter, ",");
         }
 
-        if (!negativeNumbers.isEmpty()) {
-            throw new IllegalArgumentException("Negative numbers not allowed. \nNegative numbers: " + negativeNumbers);
-        }
-
-        return sum;
+        return numbers;
     }
 
-    public static void main(String[] args) {
-        String numbers = "//[***]\n1***2,3***4\n5";
 
+    public static void main ( String[] args ) {
+        StringCalculator adder = new StringCalculator();
         try {
-            System.out.println("Result: " + new StringCalculator().add(numbers));
-        } catch (IllegalArgumentException error) {
+            System.out.println("Te summury is: " + adder.add("//[ab][abab]\n1ab2abab5ab6"));
+
+        }catch(IllegalArgumentException error){
             System.err.println("Mistake: " + error.getMessage());
         }
     }
--- lab1/src/test/java/StringCalculatorTest.java
+++ lab1/src/test/java/StringCalculatorTest.java
@@ -7,45 +7,49 @@ public class StringCalculatorTest {
     StringCalculator str = new StringCalculator();
 
     @Test
-    public void test_71() {
+    public void test_81() {
         assertEquals(0, str.add(""));
     }
 
     @Test
-    public void test_72() {
+    public void test_82() {
         assertEquals(1, str.add("1"));
     }
     @Test
-    public void test_73() {
+    public void test_83() {
         assertEquals(3, str.add("1,2"));
     }
     @Test
-    public void test_74() {
+    public void test_84() {
         assertEquals(15, str.add("1,2,3,4\n5"));
     }
     @Test
-    public void test_75(){
+    public void test_85(){
         assertThrows(IllegalArgumentException.class, () -> str.add("1,2,3,4,\n5"));
     }
     @Test
-    public void test_76() {
+    public void test_86() {
         assertEquals(15, str.add("//[*]\n1*2*3*4*5"));
     }
     @Test
-    public void test_77() {
+    public void test_87() {
         assertEquals(21, str.add("//[*]\n1,2,3*4\n5*6"));
     }
     @Test
-    public void test_78(){
+    public void test_88(){
         assertThrows(IllegalArgumentException.class, () -> str.add("//[*]\n-1,-2,3,-4,\n5"));
     }
     @Test
-    public void test_79() {
+    public void test_89() {
         assertEquals(15, str.add("//[*]\n1,2,3*4\n5*6444"));
     }
     @Test
-    public void test_710() {
+    public void test_810() {
         assertEquals(20, str.add("//[***]\n1,2,3***4\n5***5"));
+    }
+    @Test
+    public void test_811() {
+        assertEquals(24, str.add("//[;;;][!!!][###]\n1!!!2;;;3###4000!!!5,6\n7"));
     }
 
 
