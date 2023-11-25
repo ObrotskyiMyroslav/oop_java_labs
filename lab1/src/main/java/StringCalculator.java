e5a0648a1b0cba919de96102e5205208edc611e8
Myroslav <obrmyroslaw12@gmail.com>
2023-11-25 09:51:04 +0200

    step 4

--- lab1/src/main/java/StringCalculator.java
+++ lab1/src/main/java/StringCalculator.java
@@ -1,28 +1,43 @@
 public class StringCalculator {
     public int add(String numbers) {
-
         if (numbers.isEmpty()) {
             return 0;
         }
-        String[] numbers_array = numbers.split("[,\n]");
-        for(String elements : numbers_array){
+
+        String delimiter = ",";
+        String numbersWithoutDelimiter = numbers;
+
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+            if (delimiterIndex != -1) {
+                delimiter = numbers.substring(2, delimiterIndex);
+                numbersWithoutDelimiter = numbers.substring(delimiterIndex + 1);
+            }
+        }
+
+        String[] numbersArray = numbersWithoutDelimiter.split("[," + delimiter + "\n]");
+
+        int sum = 0;
+        for (String element : numbersArray) {
+            if (!element.trim().isEmpty()) {
+                sum += Integer.parseInt(element);
+            }
+        }
+        for(String elements : numbersArray){
             if(elements.trim().isEmpty()){
                 throw new IllegalArgumentException("Multiple delimiters not allowed.");
             }
         }
-        int summury = 0;
-        for (int i = 0; i < numbers_array.length; i++) {
-            summury += Integer.parseInt(numbers_array[i]);
 
-        }
-        return summury;
-
+        return sum;
     }
-    public static void main (String[]args){
-        String numbers = "1,2,3,4,\n5";
+
+    public static void main(String[] args) {
+        String numbers = "//[*]\n1,2*3,4\n5";
+
         try {
-            System.out.println("The summury is: " + new StringCalculator().add(numbers));
-        }catch(IllegalArgumentException error){
+            System.out.println("Result: " + new StringCalculator().add(numbers));
+        } catch (IllegalArgumentException error) {
             System.err.println("Mistake: " + error.getMessage());
         }
     }
--- lab1/src/test/java/StringCalculatorTest.java
+++ lab1/src/test/java/StringCalculatorTest.java
@@ -7,25 +7,33 @@ public class StringCalculatorTest {
     StringCalculator str = new StringCalculator();
 
     @Test
-    public void test_31() {
+    public void test_41() {
         assertEquals(0, str.add(""));
     }
 
     @Test
-    public void test_32() {
+    public void test_42() {
         assertEquals(1, str.add("1"));
     }
     @Test
-    public void test_33() {
+    public void test_43() {
         assertEquals(3, str.add("1,2"));
     }
     @Test
-    public void test_34() {
+    public void test_44() {
         assertEquals(15, str.add("1,2,3,4\n5"));
     }
     @Test
-    public void test_35(){
+    public void test_45(){
         assertThrows(IllegalArgumentException.class, () -> str.add("1,2,3,4,\n5"));
+    }
+    @Test
+    public void test_46() {
+        assertEquals(15, str.add("//[*]\n1*2*3*4*5"));
+    }
+    @Test
+    public void test_47() {
+        assertEquals(21, str.add("//[*]\n1,2,3*4\n5*6"));
     }
 
 }
